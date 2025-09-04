14b14698bb09e9cb0599f706883b93d1147ce870
KubaEis <eisjak01@gasos-ro.cz>
2025-09-04 10:33:31 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         List<Mazlicek> mazlicek = new ArrayList<>();
-
+        SpravaMazlicku spravaMazlicku = new SpravaMazlicku();
         /*/Mazlicek pes = new Mazlicek();
         mazlicek.add(pes);
         Mazlicek kocka = new Mazlicek();
@@ -19,6 +19,10 @@ public class Main {
         while(cislo == 1){
             System.out.println("1) Chci pridat mazlicka");
             System.out.println("2) Chci vypsat mazlicky");
+            System.out.println("3) Prumerny vek");
+            System.out.println("4) Prumerna vaha");
+            System.out.println("5) Hledani podle jmena");
+            System.out.println("6) Hledani podle druhu");
             int n = sc.nextInt();
             sc.nextLine();
             if (n > 2||n < 1){
@@ -34,6 +38,22 @@ public class Main {
                     for (int i = 0; i < mazlicek.size(); i++) {
                         System.out.println(mazlicek.get(i).toString());
                     }
+                    break;
+                case 3:
+                    spravaMazlicku.vypocetPrumernehoVeku(mazlicek);
+                    break;
+                case 4:
+                    spravaMazlicku.vypocetPrumerneVahy(mazlicek);
+                    break;
+                case 5:
+                    System.out.println("Jakeho mazlicka chcete najit?");
+                    String name = sc.nextLine();
+                    spravaMazlicku.najdiMazlickaPodleJmena(name, mazlicek);
+                    break;
+                case 6:
+                    System.out.println("Jakeho mazlicka chcete najit?");
+                    String dru = sc.nextLine();
+                    spravaMazlicku.najdiMazlickaPodleDruhu(dru, mazlicek);
                     break;
 
             }
--- src/SpravaMazlicku.java
+++ src/SpravaMazlicku.java
@@ -3,8 +3,41 @@ import java.util.List;
 
 public class SpravaMazlicku {
     public void vypocetPrumernehoVeku(List<Mazlicek> mazlicek){
+        int prumer = 0;
+        int pocet = 0;
         for(int x=0; x < mazlicek.size(); x++){
-
+            prumer = prumer+mazlicek.get(x).getVek();
+            pocet ++;
+        }
+        System.out.println("Prumer: " + prumer/pocet);
+    }
+    public void vypocetPrumerneVahy(List<Mazlicek> mazlicek){
+        double prumer = 0;
+        double pocet = 0;
+        for(int x=0; x < mazlicek.size(); x++){
+            prumer = prumer+mazlicek.get(x).getVaha();
+            pocet ++;
+        }
+        System.out.println("Prumer: " + prumer/pocet);
+    }
+    public void najdiMazlickaPodleJmena(String jmeno, List<Mazlicek> mazlicek){
+        for(int x=0; x < mazlicek.size(); x++){
+            if (mazlicek.get(x).getJmeno().equals(jmeno)){
+                System.out.println("Mazlicek se nasel:");
+                System.out.println(mazlicek.get(x));
+            }else{
+                System.out.println("Mazlicek se nenasel");
+            }
+        }
+    }
+    public void najdiMazlickaPodleDruhu(String druh, List<Mazlicek> mazlicek){
+        for(int x=0; x < mazlicek.size(); x++){
+            if (mazlicek.get(x).getDruh().equals(druh)){
+                System.out.println("Mazlicek se nasel:");
+                System.out.println(mazlicek.get(x));
+            }else{
+                System.out.println("Mazlicek se nenasel");
+            }
         }
     }
 }
